a9a762b8a6ee18b38deb7d708bf9acc1e6e54957
Stepan <Agent_77oo7@mail.ru>
2018-09-17 19:44:06 +0300

    test

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,6 @@
 /**
  * Created by Stepan on 10.08.2018.
+ * @author Basov Stepan
  */
 
 
@@ -14,68 +15,45 @@ public class Base {
             //Добавляем новую переменную choose.
             int choose = scanner.nextInt();
             //Делаем выбор, в зависимости от введенных пользователем чисел: 1 - Сумма, 2 - Вычитание, 3 - Умножение, 4 - Деление.
+             System.out.println("Введите первое число ");
+             // Водим первое число
+             double i = scanner.nextDouble();
+             // Водим второе число
+             System.out.println("Введите второе число");
+             double b = scanner.nextDouble();
+
             switch (choose) {
                 case 1:
                     System.out.println("Вы выбрали сложение!");
-                    System.out.println("Введите первое слогаемое ");
-                    // Водим первое число
-                    double i = scanner.nextDouble();
-                    // Водим второе число
-                    System.out.println("Введите второе слогаемое");
-                    double b = scanner.nextDouble();
-                    // Задаем еще одну переменную d - сумма
-                    double d = i + b;
+                     double d = i + b;
                     // Округляем переменную d до 4х знаков после запятой
                     System.out.printf("Сумма = %.4f", d);
-                    scanner.close();
-                    break;
+                     break;
                 case 2:
                     System.out.println("Вы выбрали вычитание!");
-                    System.out.println("Введите уменьшаемое ");
-                    // Водим первое число
-                    double i1 = scanner.nextDouble();
-                    // Водим второе число
-                    System.out.println("Введите вычитаемое");
-                    double b1 = scanner.nextDouble();
-                    // Задаем еще одну переменную d
-                    double d1 = i1 - b1;
+                    double d1 = i - b;
                     // Округляем переменную d до 4х знаков после запятой
                     System.out.printf("Разность = %.4f", d1);
-                    scanner.close();
                     break;
                 case 3:
                     System.out.println("Вы выбрали умножение!");
-                    System.out.println("Введите первый множитель");
-                    // Водим первое число
-                    double i2 = scanner.nextDouble();
-                    // Водим второе число
-                    System.out.println("Введите второй множитель");
-                    double b2 = scanner.nextDouble();
-                    // Задаем еще одну переменную d
-                    double d2 = i2 * b2;
+                   // Задаем еще одну переменную d
+                    double d2 = i* b;
                     // Округляем переменную d до 4х знаков после запятой
                     System.out.printf("Произведение = %.4f", d2);
-                    scanner.close();
                     break;
                 case 4:
                     System.out.println("Вы выбрали деление!");
-                    System.out.println("Введите делимое");
-                    // Водим первое число
-                    double i3 = scanner.nextDouble();
-                    // Водим второе число
-                    System.out.println("Введите делитель");
-                    double b3 = scanner.nextDouble();
-                    // Задаем еще одну переменную d
-                    double d3 = i3 / b3;
+                   // Задаем еще одну переменную d3
+                    double d3 = i / b;
                     // Округляем переменную d до 4х знаков после запятой
                     System.out.printf("Частное = %.4f", d3);
-                    scanner.close();
-                    break;
+                     break;
 
                 default:
                     System.out.println("Нет такой операции");
             }
         }
-
+       scanner.close();
     }
 }
